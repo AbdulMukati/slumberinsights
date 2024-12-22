90fe9d3b37700c6b293eaca1c05a5bd9e2a2c49c
AbdulMukati <118712206+AbdulMukati@users.noreply.github.com>
2024-12-22 14:47:34 +0800

    updated key

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://ydcjpytgpznarztystoo.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkY2pweXRncHpuYXJ6dHlzdG9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4NTM5NDAsImV4cCI6MjAyNTQyOTk0MH0.GQDJZv-URVqW0qZUvdxWUZYxR3p0JqeQ0BBxe_sLKYI";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkY2pweXRncHpuYXJ6dHlzdG9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4NDg4MjAsImV4cCI6MjA1MDQyNDgyMH0.ZrJFqRZW_dJ2kLLAOIqqjT6PEQTY6g1dLPGc6BHDyC4";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
